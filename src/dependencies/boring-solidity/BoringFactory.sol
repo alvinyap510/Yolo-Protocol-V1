5a1a92e3b02d1fd483b84199e61dc2b7c06e93d4
Alvin Yap <leaf.alvin@gmail.com>
2025-03-18 16:58:00 +0800

    dep(boring-solidity): change import path of BoringFactory.sol

--- src/dependencies/boring-solidity/BoringFactory.sol
+++ src/dependencies/boring-solidity/BoringFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./interfaces/IMasterContract.sol";
+import "@BoringSolidity/interfaces/IMasterContract.sol";
 
 // solhint-disable no-inline-assembly
 
