fe2ddff13f02ece6dcb511e8133eb94c0bd4e061
Alvin Yap <leaf.alvin@gmail.com>
2025-03-18 13:47:42 +0800

    chore(interfaces): format IFlashLoanBorrower based on Foundry format

--- src/interfaces/IFlashBorrower.sol
+++ src/interfaces/IFlashBorrower.sol
@@ -5,6 +5,11 @@ interface IFlashBorrower {
     function onFlashLoan(address _initiator, address _token, uint256 _amount, uint256 _fee, bytes calldata _data)
         external;
 
-    function onBatchFlashLoan(address _initiator, address[] calldata _tokens, uint256[] calldata _amounts, uint256[] calldata _fees, bytes calldata _data)
-        external;
+    function onBatchFlashLoan(
+        address _initiator,
+        address[] calldata _tokens,
+        uint256[] calldata _amounts,
+        uint256[] calldata _fees,
+        bytes calldata _data
+    ) external;
 }
