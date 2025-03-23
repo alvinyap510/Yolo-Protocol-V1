7a1e1bbf8b37786d7e7a17d7466cbca08980c8bc
Alvin Yap <leaf.alvin@gmail.com>
2025-03-23 19:51:23 +0800

    feat(mocks): add TestUSD for testing purpose

---
+++ src/mocks/TestUSD.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestUSD is ERC20, Ownable {
+    address public cauldron; // Only Cauldron can mint/burn
+
+    modifier onlyCauldron() {
+        require(msg.sender == cauldron, "Only Cauldron");
+        _;
+    }
+
+    constructor() ERC20("TestUSD", "TUSD") Ownable(msg.sender) {}
+
+    function setCauldron(address _cauldron) external onlyOwner {
+        require(cauldron == address(0), "Cauldron already set");
+        cauldron = _cauldron;
+    }
+
+    function mint(address to, uint256 amount) external onlyCauldron {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyCauldron {
+        _burn(from, amount);
+    }
+}
