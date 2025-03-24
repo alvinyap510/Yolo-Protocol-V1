e51b3b7ca729a714bcee017be8a75f031cc4af49
Alvin Yap <leaf.alvin@gmail.com>
2025-03-24 11:58:45 +0800

    feat(interfaces): update solidity version of IFlashBorrower and IWETH

--- src/interfaces/IFlashBorrower.sol
+++ src/interfaces/IFlashBorrower.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.0;
 
 interface IFlashBorrower {
     function onFlashLoan(address _initiator, address _token, uint256 _amount, uint256 _fee, bytes calldata _data)
