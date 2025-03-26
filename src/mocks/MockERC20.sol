d86fe677c5a2ee2c7d5506cb71234c6f906c8b3e
Alvin Yap <leaf.alvin@gmail.com>
2025-03-27 00:50:04 +0800

    fix(MockERC20.sol): fix constructor argument and add memory flag

--- src/mocks/MockERC20.sol
+++ src/mocks/MockERC20.sol
@@ -4,8 +4,8 @@ pragma solidity >=0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MintableERC20 is ERC20, Ownable {
-    constructor(string _name, string _symbol) ERC20(_name, _symbol) Ownable() {}
+contract MockERC20 is ERC20, Ownable {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) Ownable() {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
